6e5c6738f9d42c1a524b29b1a14cdeca5d34e254
Valter Barbosa <dev@valterbarbosa.com.br>
2023-08-18 16:53:37 -0300

    Rollback: suggestions box navigable from the search input or keyboard,

--- lib/src/material/field/typeahead_field.dart
+++ lib/src/material/field/typeahead_field.dart
@@ -842,21 +842,10 @@ class _TypeAheadFieldState<T> extends State<TypeAheadField<T>>
       // the style visually. However, when VO/TB are not enabled it is
       // necessary to use the Positioned widget to allow the elements to be
       // properly tappable.
-      return MediaQuery.of(context).accessibleNavigation
-          ? Semantics(
-              container: true,
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: w),
-                  child: compositedFollower,
-                ),
-              ),
-            )
-          : Positioned(
-              width: w,
-              child: compositedFollower,
-            );
+      return Positioned(
+        width: w,
+        child: compositedFollower,
+      );
     });
   }
 
